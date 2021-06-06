9cc72d9c2dd1c6de84a249722251e48a0e8c6fa0
Sahil24-lab <sahil.harriram@gmail.com>
2021-06-06 21:49:59 +1000

    updated with initial project files

---
+++ self-balancing-robot/self-balancing-robot/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * self-balancing-robot.cpp
+ *
+ * Created: 6/06/2021 9:42:31 PM
+ * Author : Sahil
+ */ 
+
+#include <avr/io.h>
+#include <iostream>
+
+int main(void)
+{
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
